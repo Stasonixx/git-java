a4e3f00e23cfa221fa182c950959ce952040c0c3
Stanio <hrutcstas@mail.ru>
2016-11-13 11:28:57 +0200

    1.2

---
+++ Java1.2/out/src/Cat.java
@@ -0,0 +1,19 @@
+
+public class Cat {
+    //charakters
+    String name;  //null
+    int age;
+    int lives;
+    double weight;
+    String color; //null
+
+    //function
+   // Cat() { //konstruktor
+    //}
+    void voice() {
+        System.out.println("mya-mya");
+    }
+    void play() {
+        System.out.println("cat playing");
+    }
+}
